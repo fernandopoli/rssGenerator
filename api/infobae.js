35a691c588d60651b9572b63438a7e03392643f4
Fernando Poli <fernandopoli@MacBook-Pro-de-Fernando>
2025-05-30 08:37:07 -0300

    Adding the new rss generator for News.

---
+++ api/infobae.js
@@ -0,0 +1,48 @@
+// api/infobae.js
+
+const axios = require('axios');
+const cheerio = require('cheerio');
+const RSS = require('rss');
+
+module.exports = async (req, res) => {
+  // Configuración inicial del feed RSS
+  const feed = new RSS({
+    title: 'Infobae',
+    description: 'Últimas noticias de Infobae.com',
+    feed_url: `${req.headers['x-forwarded-proto']}://${req.headers.host}/api/infobae`,
+    image_url: 'https://www.infobae.com/pf/resources/favicon/android-chrome-512x512.png?d=3222',
+    site_url: 'https://www.infobae.com/',
+    language: 'es',
+    pubDate: new Date(),
+  });
+
+  try {
+    // Obtener el HTML de la página de últimas noticias de Infobae
+    const { data } = await axios.get('https://www.infobae.com/');
+    const $ = cheerio.load(data);
+
+    // Extraer cada artículo de la lista de noticias
+    $('.story-card-ctn').each((i, element) => {
+      const title = $(element).find('.story-card-hl').text().trim();
+      const url = `https://www.infobae.com${$(element).attr('href')}`;
+      const image = $(element).find('img.story-card-img').attr('src');
+
+      if (title && url) {
+        feed.item({
+          title,
+          url,
+          guid: url, // Usamos la URL como identificador único
+          date,
+          image: image,
+        });
+      }
+    });
+
+    // Configurar la respuesta como XML
+    res.setHeader('Content-Type', 'text/xml');
+    res.status(200).send(feed.xml());
+  } catch (error) {
+    console.error('Error al generar el feed RSS:', error);
+    res.status(500).send('Hubo un error al procesar la solicitud.');
+  }
+};
---
+++ api/rosario3.js
@@ -0,0 +1,48 @@
+// api/rosario3.js
+
+const axios = require('axios');
+const cheerio = require('cheerio');
+const RSS = require('rss');
+
+module.exports = async (req, res) => {
+  // Configuración inicial del feed RSS
+  const feed = new RSS({
+    title: 'Rosario3',
+    description: 'Últimas noticias de Rosario3.com',
+    feed_url: `${req.headers['x-forwarded-proto']}://${req.headers.host}/api/rosario3`,
+    image_url: 'https://www.rosario3.com/__export/1559748440188/sites/rosario3/arte/v2/favicon.ico',
+    site_url: 'https://www.rosario3.com',
+    language: 'es',
+    pubDate: new Date(),
+  });
+
+  try {
+    // Obtener el HTML de la página de últimas noticias de Rosario3
+    const { data } = await axios.get('https://www.rosario3.com/seccion/ultimas-noticias/');
+    const $ = cheerio.load(data);
+
+    // Extraer cada artículo de la lista de noticias
+    $('.entry-box article').each((i, element) => {
+      const title = $(element).find('.title').text().trim();
+      const url = `https://www.rosario3.com${$(element).find('a.cover-link').attr('href')}`;
+      const image = $(element).find('img').attr('src');
+
+      if (title && url) {
+        feed.item({
+          title,
+          url,
+          guid: url, // Usamos la URL como identificador único
+          date,
+          image: image,
+        });
+      }
+    });
+
+    // Configurar la respuesta como XML
+    res.setHeader('Content-Type', 'text/xml');
+    res.status(200).send(feed.xml());
+  } catch (error) {
+    console.error('Error al generar el feed RSS:', error);
+    res.status(500).send('Hubo un error al procesar la solicitud.');
+  }
+};
