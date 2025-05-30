4a638c91af7cbb11550023d67e06c366ae4d2838
Fernando Poli <fernandopoli@MacBook-Pro-de-Fernando>
2025-05-30 14:36:00 -0300

    Removing quotes..

--- api/infobae-json.js
+++ api/infobae-json.js
@@ -25,7 +25,7 @@ module.exports = async (req, res) => {
         noticias.push({
           uid: iGuid,
           updateDate: date,
-          titleText: title.replace(/“/g, ""), 
+          titleText: title.replaceAll(/“/g, ""), 
           mainText,
           redirectionUrl: url,
           imageUrl: image,
--- api/rosario3-json.js
+++ api/rosario3-json.js
@@ -25,7 +25,7 @@ module.exports = async (req, res) => {
         noticias.push({
           uid: iGuid,
           updateDate: date,
-          titleText: title.replace(/“/g, ''),
+          titleText: title.replaceAll(/“/g, ''),
           redirectionUrl: url,
           mainText: mainText,
           imageUrl: image,
