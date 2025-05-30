695e1cac4efde93075b0ced95c6c093c8e4e48f6
Fernando Poli <fernandopoli@MacBook-Pro-de-Fernando>
2025-05-30 10:20:04 -0300

    Adding the main text.

--- api/infobae-json.js
+++ api/infobae-json.js
@@ -19,7 +19,7 @@ module.exports = async (req, res) => {
       const date = new Date(new Date().getTime() + i * 1000).toISOString();
 
       // Cuerpo del mensaje que Alexa leerá
-      const mainText = `${title}. Puedes leer más en Infobae.com`;
+      const mainText = ``;
 
       if (title && href) {
         noticias.push({
